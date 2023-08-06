216b84eb5d449194717a4f196a28a7b782597de2
Austin Sherron <austin@pryvate.email>
2023-08-05 20:57:34 -0700

    [Install] Add Google Chrome Installation Script

---
+++ install/google-chrome.sh
@@ -0,0 +1,11 @@
+# source: https://itsfoss.com/install-chrome-ubuntu/
+
+PKG="google-chrome-stable_current_amd64.deb"
+
+# download
+wget "https://dl.google.com/linux/direct/${PKG}"
+# install
+sudo dpkg -i "${PKG}"
+# clean up
+rm "${PKG}"
+
