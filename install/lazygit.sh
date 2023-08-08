eb5b64cf63c3a5831d9d610335acd15ed1804474
Austin Sherron <austin@pryvate.email>
2023-08-08 04:49:48 -0700

    [Script] Add Lazygit Installation Script

---
+++ install/lazygit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -Eeo pipefail
+
+# source: https://github.com/jesseduffield/lazygit#installation
+
+
+VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+PKG="lazygit_${VERSION}_Linux_x86_64.tar.gz"
+URL="https://github.com/jesseduffield/lazygit/releases/latest/download/${PKG}"
+OUT="lazygit-${VERSION}"
+
+# download
+[[ -f "${PKG}" ]] || curl -Lo lazygit.tar.gz "${URL}"
+# extract
+[[ -d "${OUT}" ]] || mkdir "${OUT}"
+[[ -f "${OUT}/lazygit" ]] || tar -xvf lazygit.tar.gz lazygit -C "${OUT}"
+# install
+sudo mv lazygit "${SHARED_BINS}"
+# clean up
+rm -rf "${OUT}" lazygit*.tar.gz
+
