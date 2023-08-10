918ac336a6aed697abe013ac4df20d37a7a65c39
Austin Sherron <austin@pryvate.email>
2023-08-10 15:54:57 -0700

    [Script] Add App Image Launcher Installation Script

---
+++ install/app-image-launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeo pipefail
+
+# source: https://github.com/TheAssassin/AppImageLauncher/wiki/Install-on-Ubuntu-or-Debian
+
+
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:appimagelauncher-team/stable
+sudo apt update
+sudo apt install appimagelauncher
+
