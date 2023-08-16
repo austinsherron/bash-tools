510897cf2873f5e625042b6130fac9ef0034c6bd
Austin Sherron <austin@pryvate.email>
2023-08-16 09:55:43 -0700

    [Script] Add Whatsapp-For-Linux Installation Script

---
+++ install/whatsapp-for-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeo pipefail
+
+
+VERSION="1.6.4"
+PKG="whatsapp-for-linux-${VERSION}-x86_64.AppImage"
+URL="https://github.com/eneshecan/whatsapp-for-linux/releases/download/v${VERSION}/${PKG}"
+
+[[ -f "${PKG}" ]] || wget "${URL}"
+ail-cli integrate "${PKG}"
+
