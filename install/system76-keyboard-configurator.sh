a2bc97b4020968eeee6d47cb1bb47a4951c46bd4
Austin Sherron <austin@pryvate.email>
2023-08-16 09:54:43 -0700

    [Script] Add System76 Keyboard Configurator Installation Script

---
+++ install/system76-keyboard-configurator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeo pipefail
+
+
+VERSION="1.3.9"
+PKG="keyboard-configurator-${VERSION}-x86_64.AppImage"
+URL="https://github.com/pop-os/keyboard-configurator/releases/download/v${VERSION}/${PKG}"
+
+[[ -f "${PKG}" ]] || wget "${URL}"
+ail-cli integrate "${PKG}"
+
