29a02612631304a02e8eef23123cf655e794e4b4
Austin Sherron <austin@pryvate.email>
2023-10-04 15:03:40 -0700

    [Script] Add Installation Script For Signal
    * Remove unused variable in s-search.sh
    * Update flags in s-search.sh so unrecognized variables cause script
      failures

--- install/s-search.sh
+++ install/s-search.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -Eeo pipefail
+set -Eeuo pipefail
 
 source /etc/profile.d/shared_paths.sh
 
 # source: https://github.com/zquestz/s#install (w/ significant modifications)
 
 
-VERSION="v0.6.8"
 PKG="github.com/zquestz/s"
 DST="${EXTERNAL_PKGS}/s-search"
 BIN="${DST}/s"
