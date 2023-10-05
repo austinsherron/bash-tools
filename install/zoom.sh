55ab08e4e9cae0e0103812aee6cac8a894a7cb61
Austin Sherron <austin@pryvate.email>
2023-10-05 15:08:26 -0700

    [Script] Add Zoom Installation Script

---
+++ install/zoom.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+# source: https://zoom.us/download?os=linux
+
+
+PKG="zoom_amd64.deb"
+VERSION="5.16.2.8828"
+URL="https://zoom.us/client/${VERSION}/${PKG}"
+
+if [[ "$(which zoom)" ]]; then
+    # cleanup may still be necessary
+    rm -f "${PKG}"
+    echo "zoom is already installed; exiting"
+    exit 0
+fi
+
+[[ -f "${PKG}" ]] && echo "${PKG} already downloaded" || wget "${URL}"
+sudo apt install ./${PKG}
+
+rm -f "${PKG}"
+
