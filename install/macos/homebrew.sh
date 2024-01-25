7053f08d8ad2804103c54d27dc8b77e5407e62bd
Austin Sherron <austin@pryvate.email>
2024-01-25 10:57:27 -0800

    [Script] Add MacOS Homebrew Installation Script

---
+++ install/macos/homebrew.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+
+VERSION="4.2.5"
+PKG="Homebrew-${VERSION}.pkg"
+URL="https://github.com/Homebrew/brew/releases/download/${VERSION}/${PKG}"
+DST="${HOME}/Downloads"
+
+if [[ "$(which brew)" ]]; then
+    echo "Homebrew is already installed"
+    exit 0
+fi
+
+if [[ -f "${DST}/${PKG}" ]]; then
+    echo "${DST}/${PKG} already exists"
+    exit 0
+fi
+
+cd $DST && curl -fLO "${URL}"
+
