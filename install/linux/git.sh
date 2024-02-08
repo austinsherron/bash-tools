388292803df484a1c693ad8196e07bd564288262
Austin Sherron <austin@pryvate.email>
2024-02-08 15:04:44 -0800

    [Refactor] Consolidate 1Password/CLI Installation Scripts
    * Refactor 1password.sh to improve improve readability/add logging
    * Add logging to git installation script

--- install/linux/git.sh
+++ install/linux/git.sh
@@ -5,7 +5,12 @@ set -Eeuo pipefail
 # source: https://git-scm.com/download/linux
 
 
+# NOTE: this is duplicated in the dotfiles repo: we need to install up-to-date git to
+# clone this repo
+
+echo  "[INFO] adding git-core apt repo"
 sudo add-apt-repository ppa:git-core/ppa
-sudo apt update
-sudo apt install git
+
+echo  "[INFO] installing git"
+sudo apt update && sudo apt install git
 
