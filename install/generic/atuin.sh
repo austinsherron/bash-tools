fa67ccbb08918818682488bc33da21701f4a46a3
Austin Sherron <austin@pryvate.email>
2024-03-23 10:50:04 -0700

    [Script] Update Atuin Installation Script
    * Update aforementioned script to import shell history
    * Add basic README
    * Remove unused misc script (and misc dir more generally)

--- install/generic/atuin.sh
+++ install/generic/atuin.sh
@@ -9,16 +9,30 @@ if which atuin &> /dev/null; then
     exit 0
 fi
 
+## install
+
 echo "[INFO] installing atuin"
 bash <(curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh)
 
-if [[ -z "${COMPLETION_DIR+x}" ]]; then
-    echo "[WARN] unable to install atuin completions: COMPLETION_DIR is unset"
-    exit 0
-fi
+## import existing shell history
 
 SHELL_NAME="$(basename "${SHELL}")"
+SHELL_HISTORY="${HOME}/.${SHELL_NAME}_history"
 
-echo "[INFO] installing atuin completions for shell=${SHELL_NAME}"
-atuin gen-completions --shell "${SHELL_NAME}" --out-dir "${COMPLETION_DIR}"
+[[ -z "${HISTFILE+x}" ]] && [[ -s "${SHELL_HISTORY}" ]] && export HISTFILE="${SHELL_HISTORY}"
+
+echo "[INFO] importing history for shell=${SHELL_NAME:-?} to atuin"
+
+if ! atuin import "${SHELL_NAME:-auto}"; then
+    echo "[WARN] unable to import history for shell=${SHELL_NAME:-?} to atuin"
+fi
+
+## add completions
+
+if [[ -z "${COMPLETION_DIR+x}" ]]; then
+    echo "[WARN] unable to install atuin completions: COMPLETION_DIR is unset"
+else
+    echo "[INFO] installing atuin completions for shell=${SHELL_NAME}"
+    atuin gen-completions --shell "${SHELL_NAME}" --out-dir "${COMPLETION_DIR}"
+fi
 
--- misc/random_mouse.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-from random import randint
-
-x, y = pyautogui.size()
-num_mvmts = int(input('How many mouse movements should we perform? '))
-
-for i in range(num_mvmts):
-    x, y = (randint(500, 1290), randint(325, 817))
-    pyautogui.moveTo(x, y, duration = 0.00001)
