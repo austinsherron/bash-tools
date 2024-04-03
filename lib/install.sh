45679c82fbbae8d1905c31460ac47d7f76540f3c
Austin Sherron <austin@pryvate.email>
2024-04-03 15:46:43 -0700

    [Fix] Update Source Path In Lib Install Script
    * Replace ulogger statements w/ echos, since ulogger won't be functional
      w/out the bash lib

--- lib/install.sh
+++ lib/install.sh
@@ -4,17 +4,20 @@ set -Eeuo pipefail
 
 
 if [[ -z "${LOCAL_LIB+x}" ]]; then
-    ulogger error "LOCAL_LIB is not set"
+    echo "[ERROR] LOCAL_LIB is not set"
     exit 1
 fi
 
-LIB_SRC="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 || true ; pwd -P)"
+LIB_SRC="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 || true ; pwd -P)/src"
 LIB_DST="${LOCAL_LIB}/bash"
 
+echo "SRC=${LIB_SRC}"
+echo "DST=${LIB_DST}"
+
 if [[ ! -d "${LIB_DST}" ]]; then
-    ulogger info "installing bash lib to ${LIB_DST}"
+    echo "[INFO] installing bash lib to ${LIB_DST}"
     sudo ln -s "${LIB_SRC}" "${LIB_DST}" || exit 1
 else
-    ulogger info "bash lib already exists at ${LIB_DST}"
+    echo "[INFO] bash lib already exists at ${LIB_DST}"
 fi
 
